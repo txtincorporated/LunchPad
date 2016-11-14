db112ce70107fb2833911fbfae0eefc41421e96c
Andrew Beers <andybeers@gmail.com>
2016-11-14 11:45:11 -0800

    Signup form hooked up to POST route

--- public/scripts/models/signup.js
+++ public/scripts/models/signup.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const request = require('superagent');
 const $signupForm = $('#sign-up');
 
 $signupForm.on('submit', e => {
+  const input = {};
+  input.username = $('#input-name').val();
+  input.password = $('#input-pass').val();
+  input.roles = $('#input-roles').val();
   e.preventDefault();
-  request
+  superagent
     .post('/lunch/auth/signup')
-    .type('form')
-    .end(res => {
-      console.log(res);
-      $('token-dump').append(res.body);
+    .send(input)
+    .end((err, res) => {
+      localStorage.setItem('token', res.body.token);
+      $('#token-dump').append(res.body.token);
+      console.log('stored token: ', localStorage.getItem('token'));
     });
 });
 
