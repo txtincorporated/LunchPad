7748a4e8bbb203755b6378e0636c4982bc8907a0
Nathan P. Keene <npk@txtincorporated.com>
2016-11-14 09:58:28 -0800

    Unit test Experience Model

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-'use strict';
+// 'use strict';
 
-const chai = require('chai');  //eslint-disable-line
+// const chai = require('chai');  //eslint-disable-line
 
 
-describe('it works', () => {
-  it('should work', () => {
+// describe('it works', () => {
+//   it('should work', () => {
 
-  });
-});
+//   });
+// });
