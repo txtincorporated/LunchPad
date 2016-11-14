45d3f2048b75a4fe467aed31f0ee1fb8217b14b5
Andrew Beers <andybeers@gmail.com>
2016-11-14 10:56:58 -0800

    still attempting signup

--- public/scripts/models/signup.js
+++ public/scripts/models/signup.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const request = require('superagent');
+const $signupForm = $('#sign-up');
+
+$signupForm.on('submit', e => {
+  e.preventDefault();
+  request
+    .post('/lunch/auth/signup')
+    .type('form')
+    .end(res => {
+      console.log(res);
+      $('token-dump').append(res.body);
+    });
+});
+
