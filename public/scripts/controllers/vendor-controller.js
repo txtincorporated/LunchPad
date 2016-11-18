74a84460132e4edd7e3145dbad832e065107b494
Tyler Smith <tyler.smith6891@gmail.com>
2016-11-18 10:24:23 -0800

    truncated aggregated ratings to 1 decimal point

--- public/scripts/controllers/vendor-controller.js
+++ public/scripts/controllers/vendor-controller.js
@@ -17,6 +17,12 @@ vendorController.fetchExp = function() {
       aggVendors.sort(function(a, b){
         return b.worthIt - a.worthIt;
       });
+      aggVendors.forEach(ven => {
+        ven.howFast = ven.howFast.toFixed(1);
+        ven.cost = ven.cost.toFixed(1);
+        ven.worthIt = ven.worthIt.toFixed(1);
+      });
+      // Math.floor(aggVendors[1].howFast);
       vendorView.populateHandlebars(aggVendors);
     });
 };
